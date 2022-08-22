5105a6750880f2ef843547d7d43cdfb2594157d4
Ayesha Khan <91613882+fizaayesha@users.noreply.github.com>
2022-08-22 19:42:55 +0530

    Time: 3 ms (60.95%), Space: 8.4 MB (75.20%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -11,7 +11,7 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode *root,vector <int> &ans){
+    void inorder(TreeNode *root, vector <int> &ans){
         if(root==NULL){
             return;
         }
