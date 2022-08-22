6ecffac60ea372c0f882f9d5363351b4686cb3c1
Ayesha Khan <91613882+fizaayesha@users.noreply.github.com>
2022-08-22 19:45:02 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (35.77%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -11,7 +11,7 @@
  */
 class Solution {
 public:
-    void postorder(TreeNode *root, vector <int> &ans){
+    void postorder(TreeNode *root,vector <int> &ans){
         if(root==NULL){
             return;
         }
