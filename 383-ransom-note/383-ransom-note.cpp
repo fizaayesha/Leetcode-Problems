8cab7150903a271afcd15b136bb183652b74c879
Ayesha Khan <91613882+fizaayesha@users.noreply.github.com>
2022-09-03 11:09:28 +0530

    Time: 33 ms (33.57%), Space: 8.7 MB (72.72%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
+        sort(ransomNote.begin(),ransomNote.end());
+        sort(magazine.begin(),magazine.end());
         int count[26]={0};
-        for(char ch: magazine){
+        for(char ch:magazine){
             count[ch-'a']++;
         }
-        for(char ch :ransomNote){
+        for(char ch: ransomNote){
             if(count[ch-'a']--<=0){
                 return false;
             }
