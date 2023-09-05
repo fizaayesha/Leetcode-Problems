55e0d92e3eec21faf58461fa8886237b3f5aa2e3
Ayesha Khan <91613882+fizaayesha@users.noreply.github.com>
2023-09-05 23:27:25 +0530

    Time: 4 ms (75.80%), Space: 7.9 MB (5.04%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool palindrome(string s){
+        int n=s.length();
+        int start=0,end=n-1;
+        while(start<=end){
+            if(s[start]!=s[end]){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+    string toLowerCaseString(string s){
+        int n=s.length();
+        for(int i=0;i<n;i++){
+           s[i]=tolower(s[i]);
+        }
+        return s;
+    }
+    bool isPalindrome(string s) {
+        string ans="";
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            if(s[i]>='a' && s[i]<='z' || s[i]>='A' && s[i]<='Z' || s[i]>='0' && s[i]<='9'){
+                ans.push_back(s[i]);
+            }
+        }
+         if(ans.length()==0){
+            return true;
+        }
+        // cout<<ans<<endl;
+        ans=toLowerCaseString(ans);
+        // cout<<ans<<endl;
+        return palindrome(ans);
+    }
+};
