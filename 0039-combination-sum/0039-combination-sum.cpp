26622d958bb4f2c9990e079da3725db7ea10a56a
Ayesha Khan <91613882+fizaayesha@users.noreply.github.com>
2024-08-22 15:03:16 +0530

    Time: 2 ms (82.36%), Space: 12.5 MB (81.03%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void solve(vector<int> &candidates,int target, int start, vector<int> &demo, vector<vector<int>> &ans){
+        if(target==0){
+            ans.push_back(demo);
+            return;
+        }
+        for(int i=start;i<candidates.size();i++){
+            if(i>start && candidates[i-1]==candidates[i]){
+                continue;
+            }
+            if(candidates[i]>target){
+                break;
+            }
+            demo.push_back(candidates[i]);
+            solve(candidates,target-candidates[i],i,demo,ans);
+            demo.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        int start=0;
+        vector<int> demo;
+        sort(candidates.begin(),candidates.end());
+        solve(candidates,target,start,demo,ans);
+        return ans;
+    }
+};
