b2be7272f7c6ff8415237e999442bc27cc5918ff
Ayesha Khan <91613882+fizaayesha@users.noreply.github.com>
2024-11-30 17:30:52 +0530

    Time: 0 ms (100%), Space: 6.4 MB (100%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    void solve(string digit, string output, int index, vector<string>& ans, string mapping[]){
-        if(index>=digit.length()){
+    void solve(string digits,vector<string>&ans, string output,int index, string mapping[]){
+        if(index>=digits.length()){
             ans.push_back(output);
             return;
         }
-        int number = digit[index]-'0';
+        int number=digits[index]-'0';
         string value=mapping[number];
         for(int i=0;i<value.length();i++){
             output.push_back(value[i]);
-            solve(digit,output,index+1,ans,mapping);
+            solve(digits,ans,output,index+1,mapping);
             output.pop_back();
         }
     }
     vector<string> letterCombinations(string digits) {
-        vector <string> ans;
-        if(digits.length()==0){
+        vector<string> ans;
+        string output="";
+        if(digits.length()<=0){
             return ans;
         }
-        string output = "";
         int index=0;
         string mapping[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        solve(digits,output,index,ans,mapping);
+        solve(digits,ans,output,index,mapping);
         return ans;
     }
 };
