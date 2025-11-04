4b37131cd735df6c6399eac9898cc834f48f58dc
Ayesha Khan <91613882+fizaayesha@users.noreply.github.com>
2025-11-04 10:29:53 +0530

    Time: 0 ms (100%), Space: 74.5 MB (5.14%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,13 +1,61 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int maxi=INT_MIN,sum=0;
-        int n=nums.size();
+    int solveBrute(vector<int> &nums, int n){
+        int sum=0;
+        int maxi=INT_MIN;
         for(int i=0;i<n;i++){
+            if(sum<0){
+                sum=0;
+            }
             sum+=nums[i];
-            maxi = max(maxi,sum);
-            sum=max(sum,0);
+            maxi=max(maxi,sum);
         }
         return maxi;
+    }
+    int maxi=INT_MIN;
+    void solveRec(vector<int> &nums,int n, int ind, int sum){
+        if(ind==n){
+            return;
+        }
+        if(sum<0){
+            sum=0;
+        }
+        sum+=nums[ind];
+        maxi=max(maxi,sum);
+        solveRec(nums,n,ind+1,sum);
+    }
+    void solveMemoi(vector<int> &nums, int n, int ind, int sum, vector<int> &dp){
+        if(ind==n) return;
+        if(dp[ind]!=-1){
+            return;
+        }
+        if(sum<0){
+            sum=0;
+        }
+        sum+=nums[ind];
+        maxi=max(maxi,sum);
+        dp[ind]=maxi;
+        solveMemoi(nums,n,ind+1,sum,dp);
+    }
+    int solveTab(vector<int> &nums, int n){
+        vector<int> dp(n,0);
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++){
+            dp[i]=dp[i-1]+nums[i];
+            if(dp[i]<nums[i]){
+                dp[i]=nums[i];
+            }
+        }
+        return *max_element(dp.begin(), dp.end());
+    }
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size();
+        /*
+        vector<int> dp(n+1,-1);
+        solveMemoi(nums,n,0,0,dp);
+        return maxi;
+        return solveBrute(nums,n);
+        */
+        return solveTab(nums,n);
     }
 };
